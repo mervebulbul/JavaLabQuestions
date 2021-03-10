08e1bb32a1f695ba74888bf70814b32b7ff68cb0
mervebulbul <69749924+mervebulbul@users.noreply.github.com>
2021-03-10 14:09:48 +0300

    Add files via upload

---
+++ Lab2Question3.java
@@ -0,0 +1,12 @@
+package Question3;
+
+public class question3 {
+
+	public static void main(String[] args) {
+		// TODO for loop
+		for (int i = 1; i <= 10; i++) {
+			System.out.print(i * i + " ");
+		}
+	}
+
+}
