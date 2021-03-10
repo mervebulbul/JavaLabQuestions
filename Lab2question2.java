9e73915f8d9f61a5c4e3961e8d9173427ee31f26
mervebulbul <69749924+mervebulbul@users.noreply.github.com>
2021-03-10 14:08:52 +0300

    Add files via upload

---
+++ Lab2question2.java
@@ -0,0 +1,14 @@
+package Question2;
+
+public class question2 {
+
+	public static void main(String[] args) {
+		// TODO TO PRINT ALL NUMBERS BETWEEN 1 TO 100
+
+		for (int i = 1; i <= 100; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
