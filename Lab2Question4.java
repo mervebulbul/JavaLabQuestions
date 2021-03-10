f54ddca18c7905459539a49f317814620ec791f4
mervebulbul <69749924+mervebulbul@users.noreply.github.com>
2021-03-10 14:10:45 +0300

    Add files via upload

---
+++ Lab2Question4.java
@@ -0,0 +1,15 @@
+package Question4;
+
+public class question4 {
+
+	public static void main(String[] args) {
+		// TODO calculate the sum of all numbers between 1 and 10
+
+		int sum = 0;
+		for (int i = 1; i <= 10; i++) {
+			sum = sum + i;
+		}
+		System.out.println("The sum of all numbers between 1 and 10 = " + sum);
+	}
+
+}
