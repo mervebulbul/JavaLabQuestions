7745a015ad2941805baa865a5e3ce7b8a31fdb97
mervebulbul <69749924+mervebulbul@users.noreply.github.com>
2021-04-06 21:23:06 +0300

    Add files via upload

---
+++ Lab3Question8.java
@@ -0,0 +1,17 @@
+package question8;
+
+public class question8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int line = 1; line <= 5; line++) {
+			for (int j = 1; j <= (-1 * line + 5); j++) {
+				System.out.print(" ");
+			}
+			
+			System.out.println(line);
+		}
+
+	}
+
+}
