926ce78fad2cc3d0bd7e1b62b5bd94c1731bdf99
mervebulbul <69749924+mervebulbul@users.noreply.github.com>
2021-04-06 21:21:49 +0300

    Add files via upload

---
+++ Lab3Question4.java
@@ -0,0 +1,12 @@
+package question4;
+
+public class question4 {
+	public static void largerAbsValue(int num1, int num2) {
+		System.out.println("Absolute max is " + Math.max(Math.abs(num1), Math.abs(num2)));
+	}
+
+	public static void main(String[] args) {
+		largerAbsValue(-5, 3);
+	}
+
+}
